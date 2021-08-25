08fc7ab10dd076378d922dd03c2412ce0f12bfd0
Will Knowles <will@willknowles.com>
2021-08-25 10:35:47 -0500

    server, basic routes, and dataset implemented

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
 
 const { animals } = require('./data/animals');
 
@@ -44,12 +44,26 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
 }
 
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+  }
+
 app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
         results = filterByQuery(req.query, results);
     }
     res.json(results);
+});
+
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
 });
 
 app.listen(PORT, () => {
